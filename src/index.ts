c8007744abc912769d751ac78a8188afd63a5431
Jack Humphries <jackphumphries02@gmail.com>
2023-08-17 15:22:53 +0100

    Complete frontend

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,48 @@ import { Command } from "commander";
 import { PKG_DESCRIPTION, PKG_VERSION } from "./version.js";
 import { askForIdentity } from "./identity.js";
 import chalk from "chalk";
+import { Spinner } from "cli-spinner";
 
 const program = new Command();
 
 //Grabs version and description from package.json
 program.version(PKG_VERSION).description(PKG_DESCRIPTION);
 
+const timeout = (ms: any) => new Promise((resolve) => setTimeout(resolve, ms));
+
 program.command("dump").action(async () => {
 	console.log(chalk.yellow("AWS MediaLive Cloner\n"));
 	const id = await askForIdentity();
 
+	const spinner = new Spinner("Cloning...");
+
+	spinner.setSpinnerString(18);
+	spinner.start();
+
+	await timeout(2000);
+
+	spinner.stop(true);
+
+	console.log(chalk.green("Done!"));
+
 	process.exit(0);
 });
 
-program.command("provision").action(() => {
-	console.log("Provisioning...");
+program.command("provision").action(async () => {
+	console.log(chalk.yellow("AWS MediaLive Cloner\n"));
+	const id = await askForIdentity();
+
+	const spinner = new Spinner("Provisioning...");
+
+	spinner.setSpinnerString(18);
+	spinner.start();
+
+	await timeout(2000);
+
+	spinner.stop(true);
+
+	console.log(chalk.green("Done!"));
+
 	process.exit(0);
 });
 
