cfb855a85fffc1011a5f9615e3a84dc411d6ec8a
Jack Humphries <jackphumphries02@gmail.com>
2023-08-17 14:44:23 +0100

    Added packages and meta

--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const PKG_VERSION = "1.0.0";export const PKG_DESCRIPTION = "<your-cli-description>";
+export const PKG_VERSION = "1.0.0";export const PKG_DESCRIPTION = "Clone MediaLive channels and inputs from one account to another";
